c007e532b5a933f1f79a6d21a414dd8bf3f9a240
Breyner Parada <breyner@quepar-solutions.com>
2025-09-08 18:09:10 -0500

    refactor: format code for consistency and readability

--- src/assets/HouseObj.tsx
+++ src/assets/HouseObj.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
-import * as THREE from 'three'
-import { HouseScheme } from '../constants/scheme'
+import * as THREE from "three";
+import { HouseScheme } from "../constants/scheme";
 
 export const HouseObjModel = () => {
-    const loader = new THREE.ObjectLoader()
-    const house = loader.parse(HouseScheme)
-    return <primitive object={house} scale={0.5} position={[0, 0, 0]} />
-}
+  const loader = new THREE.ObjectLoader();
+  const house = loader.parse(HouseScheme);
+  return <primitive object={house} scale={0.5} position={[0, 0, 0]} />;
+};
